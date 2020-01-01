a7bd19ab39ce1ec648e17983dfa8f5d27ff38d8f
Patrick Oryono <patricken08@gmail.com>
2020-01-01 14:38:18 +0300

    setting up CI

--- config/test.exs
+++ config/test.exs
@@ -5,7 +5,7 @@ config :meet_your_tutor, MeetYourTutor.Repo,
   username: "postgres",
   password: "postgres",
   database: "meet_your_tutor_test",
-  hostname: "localhost",
+  hostname: "db",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
