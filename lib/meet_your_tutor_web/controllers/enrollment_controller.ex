ec8925f08c51691f93e4c2d31c626f247cebb422
Patrick Oryono <patricken08@gmail.com>
2020-01-22 16:44:16 +0300

    create an enrollment

--- lib/meet_your_tutor_web/controllers/enrollment_controller.ex
+++ lib/meet_your_tutor_web/controllers/enrollment_controller.ex
@@ -1,18 +1,21 @@
 defmodule MeetYourTutorWeb.EnrollmentController do
   use MeetYourTutorWeb, :controller
 
+  alias MeetYourTutor.Repo
   alias MeetYourTutor.Courses
   alias MeetYourTutor.Courses.Enrollment
 
   action_fallback MeetYourTutorWeb.FallbackController
 
   def index(conn, _params) do
-    enrollments = Courses.list_enrollments()
+    enrollments = Courses.list_enrollments() |> Repo.preload([:class, :user])
     render(conn, "index.json", enrollments: enrollments)
   end
 
   def create(conn, %{"enrollment" => enrollment_params}) do
-    with {:ok, %Enrollment{} = enrollment} <- Courses.create_enrollment(enrollment_params) do
+    enrollment_params = Map.put(enrollment_params, "user_id", Guardian.Plug.current_resource(conn).id)
+    with {:ok, %Enrollment{} = enrollment } <- Courses.create_enrollment(enrollment_params) do
+      enrollment = enrollment |> Repo.preload([:class, :user])
       conn
       |> put_status(:created)
       |> put_resp_header("location", Routes.enrollment_path(conn, :show, enrollment))
--- lib/meet_your_tutor_web/views/enrollment_view.ex
+++ lib/meet_your_tutor_web/views/enrollment_view.ex
@@ -1,6 +1,8 @@
 defmodule MeetYourTutorWeb.EnrollmentView do
   use MeetYourTutorWeb, :view
   alias MeetYourTutorWeb.EnrollmentView
+  alias MeetYourTutorWeb.ClassView
+  alias MeetYourTutorWeb.UserView
 
   def render("index.json", %{enrollments: enrollments}) do
     %{data: render_many(enrollments, EnrollmentView, "enrollment.json")}
@@ -12,7 +14,8 @@ defmodule MeetYourTutorWeb.EnrollmentView do
 
   def render("enrollment.json", %{enrollment: enrollment}) do
     %{id: enrollment.id,
-      course_id: enrollment.course_id,
-      user_id: enrollment.user_id}
+      class: render_one(enrollment.class, ClassView, "class.json"),
+      user: render_one(enrollment.user, UserView, "user.json")
+    }
   end
 end
